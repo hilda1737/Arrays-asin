ccdba2aaba2106fff0b709809148ab26acdbc276
hilda1737 <hildamoraahh@gmail.com>
2022-03-08 22:13:24 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,51 @@
+fun main(){
+    var f = arrayOf("becky","fiona","Wendy","Hilda")
+    println(f.contentToString())
+
+
+
+
+    var b = arrayOf("Harare", "dodoma","Mumbai","jarkat")
+    cities(b)
+
+
+
+
+    var k = arrayOf(32,17,4,213,78,43,90,31,3,73,11,158,62)
+    var sum = k[2]+k[5]
+    println(sum)
+    var p = (k.indexOf(158))
+    println(p)
+
+
+
+
+    details(arrayOf("Hilda","You","Sakina"))
+
+}
+
+
+fun array(Name:Array<String>){
+    var w =(Name)
+    println(w)
+}
+
+
+fun cities(name:Array<String>){
+    name.forEach { name ->
+        println(name.capitalize())
+    }
+}
+
+
+fun numbers(number:Array<Int> ){
+    println()
+}
+
+
+
+fun details(Name:Array<String>):Array<String>{
+    var r = (Name)
+    println(Name.contentToString())
+    return r
+}
